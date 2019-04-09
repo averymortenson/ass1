48f47e3e1dbe387c5f79f1594050b227ca46676a
Avery Mortenson <49291961+averymortenson@users.noreply.github.com>
2019-04-08 23:20:24 -0700

    Update turtleprogram.h

--- turtleprogram.h
+++ turtleprogram.h
@@ -1,15 +1,47 @@
 /**
- * TODO file documentation
+ * TurtleProgram header file.
  *
- * @author TODO
- * @date TODO
+ * @author Ryan Tran
+ * @date 4/5/19
  */
 
 #ifndef TURTLEPROGRAM_H
 #define TURTLEPROGRAM_H
 
-class TurtleProgram {
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class TurtleProgram {
+	friend ostream& operator<<(ostream& output, TurtleProgram& tp);
+	
+	public:
+		// constructors & destructor
+		TurtleProgram();
+		TurtleProgram(string str);
+		TurtleProgram(string str1, string str2);
+		TurtleProgram(TurtleProgram& o);
+		~TurtleProgram();
+		
+		// operation functions
+		bool operator==(TurtleProgram& o);
+		bool operator!=(TurtleProgram& o);
+		TurtleProgram& operator=(TurtleProgram* o);
+		TurtleProgram* operator+(TurtleProgram& o);
+		TurtleProgram& operator+=(TurtleProgram& o);
+		TurtleProgram* operator*(int factor);
+		TurtleProgram& operator*=(int factor);
+		
+		// getters & setters
+		int getLength();
+		string getIndex(int index);
+		void setIndex(int index, string str);
+		
+	private:
+		string* instructions;		// array of strings.
+		int numInstructions;	// size of the instructions array.
 };
 
 #endif // TURTLEPROGRAM_H
+
